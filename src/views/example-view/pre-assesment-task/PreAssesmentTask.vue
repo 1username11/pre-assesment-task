df3d908873c055a30287998f393ad6e4f4e616e9
1username11 <ayder31101997@gmail.com>
2024-07-08 23:02:01 +0300

    refactor

--- src/views/example-view/pre-assesment-task/PreAssesmentTask.vue
+++ src/views/example-view/pre-assesment-task/PreAssesmentTask.vue
@@ -1,9 +1,8 @@
 <template>
-  <div class="p-6 space-y-4">
+  <div class="p-6 divide-y-2">
     <template v-for="(row, index) in rows" :key="index">
       <DynamicSeparatorRow :row="row" />
-
-      <div class="w-full h-1 bg-gray-200" />
+      <DynamicSeparatorRow :row="row"> - </DynamicSeparatorRow>
     </template>
   </div>
 </template>
--- src/views/example-view/pre-assesment-task/components/DynamicSeparatorRow.vue
+++ src/views/example-view/pre-assesment-task/components/DynamicSeparatorRow.vue
@@ -1,26 +1,19 @@
 <template>
-  <div class="flex flex-wrap gap-2 items-center">
-    <div v-for="(item, index) in items" :key="index" class="flex items-center">
-      <span>{{ item }}</span>
-      <template v-if="index < items.length - 1">
+  <div class="flex flex-wrap py-2">
+    <span
+      v-for="([key, value], index) in Object.entries(row)" :key="index"
+    >
+      <span class="capitalize">{{ key }} : {{ value }}</span>
+
+      <slot v-if="Object.values(row)[index + 1]">
         <span class="mx-1">•</span>
-      </template>
-    </div>
+      </slot>
+    </span>
   </div>
 </template>
 
 <script setup lang="ts">
-const props = defineProps<{
+defineProps<{
   row: IRow
 }>()
-
-const items = computed(() =>
-  Object.entries(props.row)
-    .reduce((acc, [key, value]) => {
-      if (value) {
-        acc.push(`${key.charAt(0).toUpperCase() + key.slice(1)}: ${value}`)
-      }
-      return acc
-    }, [] as string[])
-)
 </script>
